31eefcaaea2ec83ae2893ed0d8788899fcead1b0
Guillaume Lhermitte <guillaume.lhermitte@shodo.io>
2021-03-30 14:44:56 +0200

    Du passé faisons table rase

--- src/main/es/main.js
+++
@@ -1,9 +0,0 @@
-import { simpleDigits } from "../../test/es/main.test.js";
-export const lcdConverter = {
-  toLCD(number) {
-    return simpleDigits[1];
-  },
-  fromLCD(lcdStringRepresentation) {
-    return /*your code here*/ undefined;
-  }
-};
