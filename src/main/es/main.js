c895c4641023d2a0726301a8fc8358c764925413
salimchami-shodo <81433968+salimchami-shodo@users.noreply.github.com>
2021-03-30 15:12:53 +0200

    groupe 5 fizz buzz

--- src/main/es/main.js
+++ src/main/es/main.js
@@ -1,14 +1,16 @@
-function fizz (number) {
-    if (!(number % 3)) return "FIZZ";
-  }
+function fizz(number) {
+  if (!(number % 3)) return "FIZZ";
+}
 function buzz(number) {
-    if (!(number % 5)) return "BUZZ";
-  }
+  if (!(number % 5)) return "BUZZ";
+}
 function tazz(number) {
-    if (!(number % 7)) return "TAZZ";
+  if (!(number % 7)) return "TAZZ";
+}
+function retnumb(number) {
+  return number;
 }
 export const fizzBuzzer = {
-
   fizzBuzz(number) {
     return fizz(number) + buzz(number);
   },
